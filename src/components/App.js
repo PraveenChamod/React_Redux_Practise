8b64f166a189032bf5ab8f88c2301b0a13cb5069
PraveenChamod <praveenchamod23@gmail.com>
2023-08-13 20:41:24 +0530

    Initial Commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+
+import '../App.css';
+import ButtonComponents from './ButtonComponents';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Hello</h2>
+      <ButtonComponents/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ButtonComponents.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ButtonComponents extends Component {
+  render() {
+    return (
+      <div>
+        <button>Click Me</button>
+      </div>
+    )
+  }
+}
